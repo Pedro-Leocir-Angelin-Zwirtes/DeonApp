b03d04194fccf94dd667cf7eb06c545be402cff0
PedroLeocir <110078615+Pedro-Leocir-Angelin-Zwirtes@users.noreply.github.com>
2023-10-27 17:49:44 -0300

    Update main.py
    Adicionado a interface duas checkbox para que o usuário possa escolher entre calcular as horas totais e somar os valores

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import customtkinter
-import mec
+from mec import Mec
 
 customtkinter.set_appearance_mode("light")
 
@@ -7,7 +7,8 @@ class Interface():
     def __init__(self):
         self.app = customtkinter.CTk()
         self.app.title("Mecânica Deon")
-        self.app.geometry("500x370")
+        self.app.geometry("600x400")
+        self.app.resizable(False, False)
 
         self.app.grid_rowconfigure(0, weight=1)
         self.app.grid_columnconfigure((0, 1), weight=1)
@@ -15,11 +16,21 @@ class Interface():
         #Comando do botão
         def capturar_Valor():
             texto = self.textbox.get("0.0", "end")
-            mec.calcular_valor(texto, self.resultado)
+            
+            if mec_instance.STATUS_BOTAO == 2:
+                mec_instance.calcular_valor(texto, self.resultado)
+            elif mec_instance.STATUS_BOTAO == 3:
+                mec_instance.somar_horas(texto, self.resultado)
 
         def capturar_material():
             material = self.textbox2.get("0.0", "end")
-            mec.calcular_material(material, self.resultado2)
+            mec_instance.calcular_material(material, self.resultado2)
+
+        #Checkbox command
+        def checkbox_event_calcular():
+            status = self.check_var_calcular.get()
+            status_check2 = self.check_var_soma.get()
+            mec_instance.verificar_status(status, status_check2, self.checkbox_calcular, self.checkbox_soma)
 
         #Criando os widgets 1
 
@@ -30,13 +41,24 @@ class Interface():
         self.resultado2 = customtkinter.CTkLabel(self.app, text="", font=("Arial", 20), text_color='#214697')
 
 
-        self.pedro = customtkinter.CTkLabel(self.app, text="By: Pedro Leocir - Vers 2.1", font=("Arial", 14), text_color='#214697')
+        self.pedro = customtkinter.CTkLabel(self.app, text="By: Pedro Leocir - Vers 2.2", font=("Arial", 14), text_color='#214697')
 
         #Text area
         self.textbox = customtkinter.CTkTextbox(self.app, text_color='#214697', font=("Arial", 18), fg_color='#dcdcdc')
 
         #Buttons
-        self.button = customtkinter.CTkButton(self.app, text='Calcular horas', font=("Arial", 15), fg_color=("#214697"), hover_color='#131c23', command=capturar_Valor)
+        self.button = customtkinter.CTkButton(self.app, text='Calcular', font=("Arial", 15), fg_color=("#214697"), hover_color='#131c23', command=capturar_Valor)
+
+        #CheckBox das horas
+        self.check_var_calcular = customtkinter.StringVar(value="off")
+        self.checkbox_calcular = customtkinter.CTkCheckBox(self.app, text="Calcular horas", command=checkbox_event_calcular,
+                                             variable=self.check_var_calcular, onvalue="on", offvalue="off",
+                                             fg_color='#214697', border_color='#214697')
+        #CheckBox das somas das horas
+        self.check_var_soma = customtkinter.StringVar(value="off")
+        self.checkbox_soma = customtkinter.CTkCheckBox(self.app, text="Somar horas", command=checkbox_event_calcular,
+                                             variable=self.check_var_soma, onvalue="on", offvalue="off",
+                                             fg_color='#214697', border_color='#214697')
 
         #Criando os widgets 2
 
@@ -49,17 +71,21 @@ class Interface():
         #Grid
         self.label.grid(row=0, column=0, pady=10, sticky='ew', padx=20, columnspan=2)
 
-        self.textbox.grid(row=1, column=0, sticky='ew', padx=10)
-        self.textbox2.grid(row=1, column=1, sticky='ew', padx=10)
+        self.checkbox_calcular.grid(row=1, column=0, sticky='we', padx=10, pady=10)
+        self.checkbox_soma.grid(row=1, column=0, sticky='ne', padx=10, pady=10)
 
-        self.button.grid(row=2, column=0, sticky='ew', padx=20, pady=10)
-        self.button2.grid(row=2, column=1, sticky='ew', padx=20, pady=10)
+        self.textbox.grid(row=2, column=0, sticky='ew', padx=10)
+        self.textbox2.grid(row=2, column=1, sticky='ew', padx=10)
 
-        self.resultado.grid(row=3, column=0)
-        self.resultado2.grid(row=3, column=1)
+        self.button.grid(row=3, column=0, sticky='ew', padx=20, pady=10)
+        self.button2.grid(row=3, column=1, sticky='ew', padx=20, pady=10)
 
-        self.pedro.grid(row=4, column=0, columnspan=2)
+        self.resultado.grid(row=4, column=0)
+        self.resultado2.grid(row=4, column=1)
+
+        self.pedro.grid(row=5, column=0, columnspan=2)
 
 if __name__ == "__main__":
     interface = Interface()
+    mec_instance = Mec()
     interface.app.mainloop()
